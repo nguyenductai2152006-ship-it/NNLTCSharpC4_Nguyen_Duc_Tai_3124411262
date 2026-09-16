846d9a8ca1cc464b22aead0cb8aa0c3110b69a41
Tai <nguyenductai2152006@gmail.com>
2026-09-16 17:03:43 +0700

    test up file len github

---
+++ Tuan01/Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string hoTen = Console.ReadLine();
+            Console.WriteLine($"Chao ban {hoTen}!");
+        }
+    }
+}
